3dd166fb9a7f92775a579fa8b596acf8c4ba2cf4
timothyrusso <russotimothy@live.it>
2022-04-18 17:23:21 +0200

    fix: user creation validation bug fixing

--- backend/middlewares/validations.js
+++ backend/middlewares/validations.js
@@ -28,13 +28,13 @@ const validateAuthentication = celebrate({
 // User creation validation
 const validateUserCreation = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30)
+    name: Joi.string().min(2).max(30)
       .messages({
         'string.min': 'The minimum lenght of the "name" field is 2',
         'string.max': 'The maximum lenght of the "name" field is 30',
         'string.empty': 'The "name" filed must be filled in',
       }),
-    about: Joi.string().required().min(2).max(30)
+    about: Joi.string().min(2).max(30)
       .messages({
         'string.min': 'The minimum lenght of the "about" field is 2',
         'string.max': 'The maximum lenght of the "about" field is 30',
@@ -49,7 +49,7 @@ const validateUserCreation = celebrate({
       .messages({
         'string.empty': 'The "email" filed must be filled in',
       }),
-    avatar: Joi.string().required()
+    avatar: Joi.string()
       .custom(validateURL)
       .message('The "avatar" field must be a valid URL')
       .messages({
