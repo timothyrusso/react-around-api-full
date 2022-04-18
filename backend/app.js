ece3eb21a289e23d2882f4a427740e03cc129764
timothyrusso <russotimothy@live.it>
2022-04-18 11:43:15 +0200

    feat: cors headers

--- backend/app.js
+++ backend/app.js
@@ -29,6 +29,19 @@ app.use(helmet());
 
 app.use(requestLogger); // enabling the request logger
 
+app.use((req, res, next) => {
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://aroundtheus-timothyrusso.students.nomoreparties.sbs'
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  next();
+});
+
 app.use(cors());
 app.options('*', cors()); //enable requests for all routes
 
