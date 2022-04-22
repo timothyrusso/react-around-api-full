3774771821bd458caec362dcf02599adae030531
timothyrusso <russotimothy@live.it>
2022-04-22 07:58:17 +0200

    fix: bugfixing for the Card element

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -5,9 +5,9 @@ const Card = ({ card, onCardClick, onCardLike, onCardDelete }) => {
 
     const currentUser = React.useContext(CurrentUserContext);
 
-    const isOwn = card.owner._id === currentUser._id;
+    const isOwn = card.owner === currentUser._id;
     const cardDeleteButtonClassName = isOwn ? 'card__delete' : 'card__delete_hidden';
-    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    const isLiked = card.likes.some(i => i === currentUser._id);
 
     const cardLikeButtonClassName = (
         `card__like ${isLiked ? 'card__like_active' : ''}`
