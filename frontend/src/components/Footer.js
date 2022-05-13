8a40abbab7e8b2273cd91db8f94cde70e1848de8
timothyrusso <russotimothy@live.it>
2022-05-13 19:41:58 +0200

    feat: new footer contacts icons

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,15 +1,27 @@
 import React from "react";
+import linkedinIcon from "../images/linkedin.svg";
+import githubIcon from "../images/github.svg";
 
 const Footer = () => {
-
-  const date = new Date()
-  const year = date.getFullYear()
+  const date = new Date();
+  const year = date.getFullYear();
 
   return (
     <footer className="footer">
-      <p className="footer__copyright">© {year} Timothy Russo - Around The U.S.</p>
+      <p className="footer__copyright">
+        © {year} Timothy Russo - Around The U.S.
+      </p>
+      <a href="https://github.com/timothyrusso" target="_blank">
+        <img src={githubIcon} className="footer__icon"></img>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/russotimothysoftwareengineer/"
+        target="_blank"
+      >
+        <img src={linkedinIcon} className="footer__icon"></img>
+      </a>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
