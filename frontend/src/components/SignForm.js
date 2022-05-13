4f2746dec65f2956529859b6513b6f8c4d7ee2ad
timothyrusso <russotimothy@live.it>
2022-05-13 18:40:00 +0200

    feat: new intro graphics

--- frontend/src/components/SignForm.js
+++ frontend/src/components/SignForm.js
@@ -1,15 +1,39 @@
 import React from "react";
 
 const SignForm = ({ title, handleSubmit, handleChange, inputs }) => {
-
-    return (
-        <form className="sign-form" onSubmit={handleSubmit}>
-            <h2 className="sign-form__title">{title}</h2>
-            <input type="email" className="sign-form__input" id="email-input" name="email" placeholder="Email" onChange={handleChange} value={inputs.email}></input>
-            <input type="password" className="sign-form__input" id="password-input" name="password" placeholder="Password" onChange={handleChange} value={inputs.password}></input>
-            <button className="sign-form__submit">{title}</button>
-        </form>
-    )
-}
+  return (
+    <form className="sign-form" onSubmit={handleSubmit}>
+      <div className="sign-form__intro">
+        <div className="circle rotation"></div>
+        <div className="square rotation-inverse"></div>
+        <p className="sign-form__intro-paragraph">
+          Original photos for original places. Share the most original.
+        </p>
+      </div>
+      <div className="sign-form__wrapper">
+        <h2 className="sign-form__title">{title}</h2>
+        <input
+          type="email"
+          className="sign-form__input"
+          id="email-input"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+          value={inputs.email}
+        ></input>
+        <input
+          type="password"
+          className="sign-form__input"
+          id="password-input"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+          value={inputs.password}
+        ></input>
+        <button className="sign-form__submit">{title}</button>
+      </div>
+    </form>
+  );
+};
 
 export default SignForm;
