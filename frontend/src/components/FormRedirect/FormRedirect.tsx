a803ba4e6f159d668b55c87e66f25cf7c58de314
timothyrusso <russotimothy@live.it>
2023-11-18 15:26:33 +0100

    feat: turn form redirect component in typescript second part

--- frontend/src/components/FormRedirect/FormRedirect.tsx
+++ frontend/src/components/FormRedirect/FormRedirect.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-const FormRedirect = ({ text, textLink, redirectElement }) => {
-    return (
-        <p className="form-redirect">{text}<Link to={`/${redirectElement}`} className="form-redirect_type_link">{textLink}</Link></p>
-    )
-}
+export type FormRedirectProps = {
+  text: string;
+  textLink: string;
+  redirectElement: string;
+};
+
+const FormRedirect = ({
+  text,
+  textLink,
+  redirectElement,
+}: FormRedirectProps) => {
+  return (
+    <p className="form-redirect">
+      {text}
+      <Link to={`/${redirectElement}`} className="form-redirect_type_link">
+        {textLink}
+      </Link>
+    </p>
+  );
+};
 
 export default FormRedirect;
