08b77718b1aa18a72d65e6d335088a682531f3fe
Timothy Russo <russotimothy@live.it>
2023-11-21 17:13:49 +0100

    feat: migrate to typescript the FormRedirect component

--- frontend/src/components/FormRedirect/FormRedirect.tsx
+++ frontend/src/components/FormRedirect/FormRedirect.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
-const FormRedirect = ({ text, textLink, redirectElement }) => {
+export type FormRedirectProps = {
+    text: string;
+    textLink: string;
+    redirectElement: string;
+}
+
+const FormRedirect = ({ text, textLink, redirectElement }: FormRedirectProps) => {
     return (
         <p className="form-redirect">{text}<Link to={`/${redirectElement}`} className="form-redirect_type_link">{textLink}</Link></p>
     )
