f4c5aadd05fcc2e93b1804fe5b74be552b3716f2
Timothy Russo <russotimothy@live.it>
2023-11-21 17:26:17 +0100

    fix: formredirect file

--- frontend/src/components/FormRedirect/FormRedirect.tsx
+++ frontend/src/components/FormRedirect/FormRedirect.tsx
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import { Link } from 'react-router-dom';
-
-export type FormRedirectProps = {
-    text: string;
-    textLink: string;
-    redirectElement: string;
-}
-
-const FormRedirect = ({ text, textLink, redirectElement }: FormRedirectProps) => {
-    return (
-        <p className="form-redirect">{text}<Link to={`/${redirectElement}`} className="form-redirect_type_link">{textLink}</Link></p>
-    )
-}
-
-export default FormRedirect;
-=======
-import React from 'react';
 import { Link } from 'react-router-dom';
 
 export type FormRedirectProps = {
@@ -41,4 +22,3 @@ const FormRedirect = ({
 };
 
 export default FormRedirect;
->>>>>>> a803ba4e6f159d668b55c87e66f25cf7c58de314
