d97bd0454c8192ab3115002f67fdc5a12ec0a36c
timothyrusso <russotimothy@live.it>
2023-11-25 15:59:05 +0100

    feat: start migration to typescript for the register component

--- frontend/src/components/Register/Register.tsx
+++ frontend/src/components/Register/Register.tsx
@@ -1,34 +1,46 @@
-import React, { useState } from "react";
-import SignForm from "../SignForm/SignForm";
-import FormRedirect from "../FormRedirect/FormRedirect";
+import React, { useState } from 'react';
+import SignForm from '../SignForm/SignForm';
+import FormRedirect from '../FormRedirect/FormRedirect';
 
-const Register = ({ handleRegisterSubmit }) => {
+export type RegisterProps = {
+  handleRegisterSubmit: (password: string, email: string) => void;
+};
 
-    const [inputs, setInputs] = useState({
-        email: '',
-        password: ''
-    });
+const Register = ({ handleRegisterSubmit }: RegisterProps) => {
+  const [inputs, setInputs] = useState({
+    email: '',
+    password: '',
+  });
 
-    const handleChange = (evt) => {
-        const { name, value } = evt.target;
-        setInputs((prevState) => ({
-            ...prevState,
-            [name]: value,
-        }))
-    }
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    setInputs((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
-    const handleSubmit = (evt) => {
-        const { email, password } = inputs;
-        evt.preventDefault();
-        handleRegisterSubmit(password, email)
-    }
+  const handleSubmit = (evt) => {
+    const { email, password } = inputs;
+    evt.preventDefault();
+    handleRegisterSubmit(password, email);
+  };
 
-    return (
-        <>
-            <SignForm title="Sign up" handleSubmit={handleSubmit} handleChange={handleChange} inputs={inputs}/>
-            <FormRedirect text={"Already a member? "} textLink="Log in here!" redirectElement="signin" />
-        </>
-    )
-}
+  return (
+    <>
+      <SignForm
+        title="Sign up"
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+        inputs={inputs}
+      />
+      <FormRedirect
+        text={'Already a member? '}
+        textLink="Log in here!"
+        redirectElement="signin"
+      />
+    </>
+  );
+};
 
 export default Register;
